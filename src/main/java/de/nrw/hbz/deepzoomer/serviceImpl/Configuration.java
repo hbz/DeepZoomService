fdc5bde184f53a51caf7bb0ab773525ed33af6cc
Andres Quast <quast@hbz-nrw.de>
2015-08-17 15:10:19 +0200

    Initial Commit

---
+++ src/main/java/de/nrw/hbz/deepzoomer/serviceImpl/Configuration.java
@@ -0,0 +1,164 @@
+/**
+ * 
+ */
+package de.nrw.hbz.deepzoomer.serviceImpl;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+
+/**
+ * @author aquast
+ *
+ */
+public class Configuration {
+	
+
+	// Initialize logger object 
+	private static Logger log = Logger.getLogger(Configuration.class);
+
+	private static Properties defProp = new Properties();
+	private static Properties sysProp = null;
+	
+	private static String serviceUrl = null;
+	private static String tempDirUrl = null;
+	private static String resultDirUrl = null;
+
+	static String resultDirPath = null;
+	static String tempDirPath = null;
+
+	static{
+		initLog();
+		setDefaultProp();
+		loadConfigurationFile();
+		setResultDirPath();
+		setTempDirPath();
+		setServiceUrl();
+		setResultDirUrl();
+		setTempDirUrl();
+	}
+
+	private static void setDefaultProp(){
+		defProp.setProperty("host", "nyx.hbz-nrw.de");
+		defProp.setProperty("port", "8080");
+		defProp.setProperty("path", "PdfAConverter");
+		defProp.setProperty("tempDir", "temp");
+		defProp.setProperty("resultDir", "result");
+		defProp.setProperty("userDir", "ulbm");
+		defProp.setProperty("workingDir", "/srv/tomcat6/webapps/");
+		
+		
+	}
+
+	private static void setTempDirUrl(){
+		 tempDirUrl = serviceUrl + sysProp.getProperty("tempDir") + "/";
+	}
+	
+	private static void setResultDirUrl(){
+		resultDirUrl = serviceUrl + sysProp.getProperty("resultDir")  + "/";
+		
+	}
+	
+	private static void setServiceUrl(){
+		serviceUrl = "http://" + sysProp.getProperty("host") + ":" 
+				+ sysProp.getProperty("port") + "/"
+				+ sysProp.getProperty("path") + "/"; 
+	}
+
+	private static void setResultDirPath(){
+		//resultDirPath = System.getProperty("user.dir") + sysProp.getProperty("resultDir") + "/";
+		resultDirPath = sysProp.getProperty("workingDir") + sysProp.getProperty("resultDir") + "/";
+	}
+
+	private static void setTempDirPath(){
+		//tempDirPath = System.getProperty("user.dir") + sysProp.getProperty("tempDir") + "/";
+		tempDirPath = sysProp.getProperty("workingDir") + sysProp.getProperty("tempDir") + "/";
+	}
+
+	public static void loadConfigurationFile(){
+        sysProp = new Properties(defProp);
+        try {
+            InputStream propStream = new Configuration().getClass().getResourceAsStream("/conf/pdfaService.cfg");
+            if (propStream == null) {
+                throw new IOException("Error loading configuration: /conf/pdfa.conf not found in classpath");
+            }else{
+                sysProp.load(propStream);
+            }
+        } catch (Exception e) {
+        	log.warn(e);
+        }
+
+	}
+	
+	
+	/**
+	 *  Method for initiate Logging System which include Logger 
+	 *  Configuration from log4j.properties 
+	 *  @author Andres Quast 
+	 */
+	public static void initLog() {
+		try {
+			PropertyConfigurator.configure(new Configuration().readLogProperties());
+			
+		} catch (IOException e) {
+			log.info(e);
+		}
+	}
+	
+	/**
+	 * <p><em>Title: </em></p>
+	 * <p>Description: Method loads log properties from file if accessible</p>
+	 * 
+	 * @return
+	 * @throws IOException 
+	 */
+	private Properties readLogProperties() throws IOException {
+		Properties logProps = new Properties();
+        InputStream propStream = this.getClass().getResourceAsStream("/conf/log4j.properties");
+        if (propStream == null) {
+             throw new IOException("failed to load log4j.properties: file not found");
+            }else{
+                logProps.load(propStream);
+                System.out.println("read log4j-configuration");
+            }
+        return logProps;       
+	}
+
+	public static String getTempDirPath() {
+		return tempDirPath;
+	}
+
+	public static String getResultDirPath() {
+		return resultDirPath;
+	}
+
+	public static String getServiceUrl() {
+		return serviceUrl;
+	}
+
+	public static String getTempDirUrl() {
+		return tempDirUrl;
+	}
+
+	public static String getResultDirUrl() {
+		return resultDirUrl;
+	}
+
+	public static String getWorkingDir(){
+		return sysProp.getProperty("workingDir");
+	}
+
+	public static String getUserDir(){
+		return sysProp.getProperty("userDir");
+	}
+
+}
---
+++ src/main/java/de/nrw/hbz/deepzoomer/serviceImpl/VipsRunner.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package de.nrw.hbz.deepzoomer.serviceImpl;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author aquast
+ *
+ */
+public class VipsRunner {
+	// Initiate Logger for PilotRunner
+	private static Logger log = Logger.getLogger(VipsRunner.class);
+	
+	private String exitStateStr= null; 	
+	private String stoutStr = null;
+	
+	/**
+	 * <p><em>Title: </em></p>
+	 * <p>Description: Method creates the command line string with all parameters given. 
+	 * Then executes the shell command </p>
+	 * 
+	 * @param paramString
+	 * @param fileName 
+	 */
+	public void executeVips(String paramString, String fileName){
+		// call to execute VIPS
+		
+		// Complete execute String 
+		String programPath = new String("vips"); 
+		String defaultParams = new String("dzsave " 
+				 + "--suffix .jpeg[90] "
+				);
+		String executeString = new String(programPath + " " 
+				+ defaultParams 
+				+ paramString 
+				+ " " + Configuration.getResultDirPath() + fileName 
+				+ " " + Configuration.getTempDirPath() + fileName); 
+
+		log.debug("The execute String: " + executeString);
+		try{
+			//Process proc = java.lang.Runtime.getRuntime().exec("echo " + executeString);
+			Process proc = java.lang.Runtime.getRuntime().exec(executeString);
+			int exitState = proc.waitFor();
+			InputStream stout = proc.getInputStream();
+            InputStreamReader isr = new InputStreamReader(stout);
+            BufferedReader br = new BufferedReader(isr);
+            String line = null;
+            StringBuffer lineBuffer = new StringBuffer();
+            while ((line = br.readLine()) != null){
+                lineBuffer.append(line + "\n");
+            }
+            log.debug("STOUT: " + lineBuffer.toString());
+            log.info("Exit State: " + exitState);
+            stoutStr = lineBuffer.toString();
+            exitStateStr = Integer.toString(exitState);
+		}catch(Exception Exc){
+			log.error(Exc);
+		}
+		
+		//unlink temp file
+		if(new File(Configuration.getTempDirPath() + fileName).isFile()){
+			new File(Configuration.getTempDirPath() + fileName).delete();
+		}
+		// TODO: das Ausführen von VIPS kann etwas dauern... was mache
+		// ich um festzustellen, dass VIPS seine Arbeit beendet hat? 		
+	}
+
+	public String getStoutStr(){
+		return stoutStr;
+	}
+
+	public String getExitStateStr(){
+		return exitStateStr;
+	}
+}
+}
