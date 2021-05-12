54b160643f049b4adf5c5fe037150a2413fc611e
Andres Quast <quast@hbz-nrw.de>
2021-05-12 09:48:25 +0200

    make code neat according codacy

--- src/main/webapp/js/pictureChoser.js
+++ src/main/webapp/js/pictureChoser.js
@@ -1,27 +1,28 @@
-jQuery(document).ready(getDziName());
+function getDziName() {
 
-function getDziName(){
+  var viewer = OpenSeadragon({
+    id: "openseadragon1",
+    prefixUrl: "openseadragon-bin-2.0.0/images/",
+    tileSources: [{ Image: { Url: "http://192.168.178.30/opensd/Grenzbaude2400_files/" } }],
+    showNavigator: "true",
+  });
 
-    var viewer = OpenSeadragon({
-        id: "openseadragon1",
-        prefixUrl: "openseadragon-bin-2.0.0/images/",
-        tileSources: [ {Image: {Url: "http://192.168.178.30/opensd/Grenzbaude2400_files/"}}],
-        showNavigator: "true",
-      });
-
-  jQuery('li').each(function(){
+  jQuery("li").each(function() {
     var dziName = jQuery(this).html();
 
-    jQuery(this).click(function(){
+    jQuery(this).click(function() {
       viewer.destroy();
       viewer = OpenSeadragon({
         id: "openseadragon1",
         prefixUrl: "openseadragon-bin-2.0.0/images/",
-        tileSources: dziName+".dzi",
+        tileSources: dziName + ".dzi",
         showNavigator: "true",
       });
-      
+
     });
   });
 }
+
+jQuery(document).ready(getDziName());
+
 
